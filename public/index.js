12b73b0d650b3380c5ca55ea6c58307267d24417
Mary May <mayma@flip1.engr.oregonstate.edu>
2019-06-06 22:43:55 -0700

    took me SO LONG to fricken do ONE THING

--- public/index.js
+++ public/index.js
@@ -1,6 +1,11 @@
+
 var modal = document.getElementById('create-flashcard-modal');
 var createCardButton = document.getElementsByClassName('create-new-card')[0];
 var closeModal = document.getElementsByClassName('modal-cancel-flashcard')[0];
+var card = document.getElementsByClassName('flash-card')[0];
+
+var front = document.getElementsByClassName("front")[0];
+var back = document.getElementsByClassName("back")[0];
 
 createCardButton.addEventListener('click', function (event) {
   console.log("== button clicked");
@@ -9,4 +14,16 @@ createCardButton.addEventListener('click', function (event) {
 
 closeModal.addEventListener('click', function (event) {
   modal.style.display = "none";
+});
+
+card.addEventListener('click', function(event) {
+  console.log("== card clicked");
+  if(front.style.visibility != "hidden"){
+    front.style.visibility = "hidden";
+    back.style.visibility = "visible";
+  }
+  else{
+    front.style.visibility = "visible";
+    back.style.visibility = "hidden";
+  }
 });
