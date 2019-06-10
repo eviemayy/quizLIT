894b0fef1251711a073b7921150f7350fa2e3ade
Mary May <mayma@flip3.engr.oregonstate.edu>
2019-06-09 18:43:58 -0700

    added app.post server stuff

--- server.js
+++ server.js
@@ -30,6 +30,30 @@ app.get('*', function(req, res) {
     res.status(404).render('404');
 });
 
+/*app.post('/:setName', function (req, res, next) {
+});*/
+
+app.post('/:setName/addCard', function (req, res, next) {
+  if (req.body && req.body.term && req.body.defintion) {
+    var name = req.params.setName.toLowerCase();
+    console.log("req.body.term: ", req.body.term, "req.body.definition:", req.body.definition)
+    if (cardData[name]) {
+      cardData[name].cards.push({
+        term: req.body.term,
+        defintion: req.body.defintion
+      });
+      console.log("== cards for", name, ":", cardData[name].cards);
+      res.status(200).send("Card successfully added");
+    } else {
+      next();
+    }
+  } else {
+    res.status(400).send({
+      error: "Request body needs a term and defintion."
+    });
+  }
+});
+
 app.listen(port, function() {
     console.log("== Server is listening on port", port);
 });
