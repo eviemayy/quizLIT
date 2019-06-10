87c932716afe043edae9b56fa09c3849f41f121b
Mary May <mayma@flip3.engr.oregonstate.edu>
2019-06-09 17:56:39 -0700

    homepage added

--- server.js
+++ server.js
@@ -13,10 +13,6 @@ app.use(express.static('public'));
 
 var cardData = require('./cardData');
 
-app.get('/', function (req, res) {
-    res.status(200).render('main');
-});
-
 app.get('/:setName', function (req, res, next) {
   var name = req.params.setName.toLowerCase();
   if (cardData[name]) {
@@ -24,6 +20,10 @@ app.get('/:setName', function (req, res, next) {
   } else {
     next();
   }
+});
+
+app.get('/', function (req, res) {
+    res.status(200).render('homePage');
 });
 
 app.get('*', function(req, res) {
