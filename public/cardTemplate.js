d9157723089065816e03eac7984ef632e25545f3
blakele <49256995+blakele@users.noreply.github.com>
2019-06-11 21:45:58 -0700

    cooler colors

--- public/cardTemplate.js
+++ public/cardTemplate.js
@@ -3,7 +3,6 @@
 templates['card'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-
   return "<article class=\"flash-card\">\r\n    <p class=\"front term visible\"> "
     + alias4(((helper = (helper = helpers.term || (depth0 != null ? depth0.term : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"term","hash":{},"data":data}) : helper)))
     + "</p>\r\n    <p class=\"back definition hidden\"> "
