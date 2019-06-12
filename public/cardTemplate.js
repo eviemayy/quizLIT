d38311e39c217316175d0169318d8102fd8b0e19
parsonsz <parsonsz@oregonstate.edu>
2019-06-11 21:23:58 -0700

    foregoing mongo for now

--- public/cardTemplate.js
+++ public/cardTemplate.js
@@ -3,9 +3,9 @@
 templates['card'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<article class=\"flash-card\">\n    <p class=\"front term\"> "
+  return "<article class=\"flash-card\">\n    <p class=\"front term visible\"> "
     + alias4(((helper = (helper = helpers.term || (depth0 != null ? depth0.term : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"term","hash":{},"data":data}) : helper)))
-    + "</p>\n    <p class=\"back definition\"> "
+    + "</p>\n    <p class=\"back definition hidden\"> "
     + alias4(((helper = (helper = helpers.definition || (depth0 != null ? depth0.definition : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"definition","hash":{},"data":data}) : helper)))
     + " </p>\n</article>\n";
 },"useData":true});
